1b5de849015e278407a2fc7d5a57f6dd5a494270
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-11-11 13:01:13 -0700

    migrating to new Svelte ~ scratch

--- src/components/layout.ts
+++
@@ -1,3 +0,0 @@
-import Header from "./layout/Header/Header.svelte";
-
-export { Header };
--- src/stories/index.ts
+++
@@ -1,5 +0,0 @@
-import Button from './Button/Button.svelte';
-import ThemePicker from './ThemePicker/ThemePicker.svelte';
-import Anchor from './Anchor/Anchor.svelte';
-
-export { Anchor, Button, ThemePicker };
