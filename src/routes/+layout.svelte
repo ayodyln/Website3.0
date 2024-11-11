8c31126c1225d5f787a6536edd2518c0e1fa9f08
ayodyln <78442693+ayodyln@users.noreply.github.com>
2024-11-11 15:46:19 -0700

    updated readme and added my scss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-	import { i18n } from '$lib/i18n';
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
-	let { children } = $props();
+	import '../scss/style.scss';
 </script>
 
-<ParaglideJS {i18n}>
-	{@render children()}
-</ParaglideJS>
+<slot />
