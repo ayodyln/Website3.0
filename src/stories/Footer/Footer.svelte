4eec5fe57e84a5f6d77987cbc2bf98147125e381
ayodyln <78442693+ayodyln@users.noreply.github.com>
2025-09-09 23:11:09 -0600

    fresh start with sveltekit

--- src/stories/Footer/Footer.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import './footer.scss';
-</script>
-
-<footer class="site-footer">
-	<div class="site-footer__content">
-		&copy; Dylan Smith
-	</div>
-</footer>
--- src/stories/stories.ts
+++
@@ -1,5 +0,0 @@
-import Header from './Header/Header.svelte';
-import Footer from './Footer/Footer.svelte';
-import Button from './Button/Button.svelte';
-
-export { Header, Footer, Button };
